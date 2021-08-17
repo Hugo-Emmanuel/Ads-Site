d66f08892dc941a23544dbaf864303660d38027a
Emmanuel Emejulu <emmanueljunior433@gmail.com>
2021-08-17 01:10:27 +0100

    Add files via upload

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ let year, month, date, url, xhr, res;
 year = new Date().getFullYear();
 month = new Date().getMonth()+1;
 date = new Date().getDate();
-url = `https://api3.adsterratools.com/publisher/db5da897145c936dcf8a88f70198dd1b/stats.json?domain=1593216&country=ng&placement=16398057&start_date=2021-08-11&finish_date=${year}-${month}-${date}&group_by=date`;
+url = "https://api3.adsterratools.com/publisher/db5da897145c936dcf8a88f70198dd1b/stats.json?domain=1593216&country=ng&placement=16398057&start_date=2021-08-11&finish_date="+year+"-"+month+"-"+date+"&group_by=date";
 xhr = new XMLHttpRequest();
 xhr.open("GET", url);
 xhr.onreadystatechange = ()=> {
